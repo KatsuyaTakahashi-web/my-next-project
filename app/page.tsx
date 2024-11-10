5eaf1041ecb3a128cbd23a32c1b07e669ba71018
KatsuyaTakahashi-web <moko_151_27@yahoo.co.jp>
2024-11-10 11:29:41 +0900

    p48まで

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,24 @@
 import styles from "./page.module.css";
+import Image from "next/image";
 
 export default function Home() {
-  // Javascriptの定数や式などを書くことができる
   const name = "世界";
 
-  // HTMLのようなマークアップを書くことができる
-  // Javascriptを書いたり、参照する場合は{}で囲む
   return (
-    <div>
-      <h1 className={styles.title}>テクノロジーの力で{name}を変える</h1>
-      <p className={styles.description}>
-        私たちは市場をリードしているグローバルテックカンパニーです。
-      </p>
-    </div>
+    <section className={styles.top}>
+      <div>
+        <h1 className={styles.title}>テクノロジーの力で{name}を変える</h1>
+        <p className={styles.description}>
+          私たちは市場をリードしているグローバルテックカンパニーです。
+        </p>
+      </div>
+      <Image
+        className={styles.bgimg}
+        src="/img-mv.jpg"
+        alt=""
+        width={4000}
+        height={1200}
+      />
+    </section>
   );
 }
