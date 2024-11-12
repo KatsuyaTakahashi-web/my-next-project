39aebd15c4547e9bdd51efaf9a1c9e178d75c233
KatsuyaTakahashi-web <moko_151_27@yahoo.co.jp>
2024-11-12 23:02:27 +0900

    4-2まで

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 import Styles from "./index.module.css";
 
 export default function Header() {
   return (
     <header className={Styles.header}>
-      <a href="/" className={Styles.logoLink}>
+      <Link href="/" className={Styles.logoLink}>
         <Image
           src="logo.svg"
           alt="SAMPLE"
@@ -13,7 +14,20 @@ export default function Header() {
           height={133}
           priority
         />
-      </a>
+      </Link>
+      <nav className={Styles.nav}>
+        <ul className={Styles.items}>
+          <li>
+            <Link href="/news">ニュース</Link>
+          </li>
+          <li>
+            <Link href="/members">メンバー</Link>
+          </li>
+          <li>
+            <Link href="/contact">お問い合わせ</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
